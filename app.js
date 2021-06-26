20428fc661476d455fa4003973181ae01354a11a
amaury <amaufischer@gmail.com>
2021-06-26 19:56:06 +0200

    fix heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http")
 const { buildCommonMessage, sendFcmMessage } = require("./src/FCMService")
 
-const hostname = "127.0.0.1"
+const hostname = "0.0.0.0"
 const port = 3000
 
 const server = http.createServer((req, res) => {
